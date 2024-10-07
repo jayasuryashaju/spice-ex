d2c71651cf5915811af36e8d980fea62ee6ca08f
jayasuryashaju <jayasuryashaju3031@gmail.com>
2024-10-07 15:16:27 +0530

    updated STATIC_ROOT

--- spiceex/settings/production.py
+++ spiceex/settings/production.py
@@ -18,7 +18,7 @@ DATABASES = {
 
 # Use the actual static and media file configuration
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
